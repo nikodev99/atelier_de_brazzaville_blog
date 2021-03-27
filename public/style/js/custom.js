abf1f9742319f406d420c95fa3227ca3675d1156
nikodev99 <nikhe.niama99@gmail.com>
2021-03-27 07:19:24 +0100

    Raw view modify. from cascadia to atelier de brazzaville

--- public/style/js/custom.js
+++ public/style/js/custom.js
@@ -5,20 +5,20 @@
     http://themeforest.net/user/wpdestek
 **************************************/
 
-(function($) {
+(function ($) {
     "use strict";
-    $(document).ready(function() {
-        $('#nav-expander').on('click', function(e) {
+    $(document).ready(function () {
+        $('#nav-expander').on('click', function (e) {
             e.preventDefault();
             $('body').toggleClass('nav-expanded');
         });
-        $('#nav-close').on('click', function(e) {
+        $('#nav-close').on('click', function (e) {
             e.preventDefault();
             $('body').removeClass('nav-expanded');
         });
     });
 
-    $(function() {
+    $(function () {
         $('[data-toggle="tooltip"]').tooltip()
     })
 
@@ -26,19 +26,19 @@
         interval: 4000
     })
 
-    $(window).load(function() {
+    $(window).load(function () {
         $("#preloader").on(500).fadeOut();
         $(".preloader").on(600).fadeOut("slow");
     });
 
-    jQuery(window).scroll(function(){
+    jQuery(window).scroll(function () {
         if (jQuery(this).scrollTop() > 1) {
             jQuery('.dmtop').css({bottom:"25px"});
         } else {
             jQuery('.dmtop').css({bottom:"-100px"});
         }
     });
-    jQuery('.dmtop').click(function(){
+    jQuery('.dmtop').click(function () {
         jQuery('html, body').animate({scrollTop: '0px'}, 800);
         return false;
     });
@@ -46,7 +46,8 @@
 })(jQuery);
 
 
-function openCategory(evt, catName) {
+function openCategory(evt, catName)
+{
     // Declare all variables
     var i, tabcontent, tablinks;
 
